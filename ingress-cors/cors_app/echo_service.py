0370aa0743b9cce35fad1f392e34bd0237ebc916
Yilei Zhang <yilei.zhang@snowflake.com>
2025-01-15 13:39:45 -0800

    simplify to localhost, no text box, PAT

--- ingress-cors/cors_app/echo_service.py
+++ ingress-cors/cors_app/echo_service.py
@@ -37,26 +37,40 @@ app = Flask(__name__)
 
 @app.get("/healthcheck")
 def readiness_probe():
-    return "I'm ready!"
+    response = make_response("I'm ready!")
+
+    # Sending both Custom-Header-X and Custom-Header-Y but only Custom-Header-X is exposed
+    response.headers['Custom-Header-X'] = 'foo'
+    response.headers['Custom-Header-Y'] = 'bar'
+    return response
 
 
 @app.get("/get")
 def get_func():
-    response = make_response({"data": "GET success!"})
+    response = make_response("GET success!")
     response.headers['Access-Control-Allow-Origin'] = 'https://b3efa44-sfengineering-prod2-snowservices-test2.snowflakecomputing.app'
     response.headers['Access-Control-Allow-Credentials'] = True
     response.headers['Access-Control-Allow-Headers'] = 'Fake-Header-X,Fake-Header-Y,Fake-Header-Z'
-    logger.debug(f'Sending response: {response.json}')
+
+    # Sending both Custom-Header-X and Custom-Header-Y but only Custom-Header-X is exposed
+    response.headers['Custom-Header-X'] = 'foo'
+    response.headers['Custom-Header-Y'] = 'bar'
     return response
 
 
 @app.route('/echo', methods=['POST'])
-def echo():
-    # Get the JSON payload from the request
+def echo(): 
     payload = request.get_json()
+
+    # Making sure that Access-Control-Allow-Headers appear
+    assert "Custom-Header-A" in request.headers, request.headers
     
-    # Return the payload as a JSON response
-    return str(payload), 200
+    response = make_response(payload)
+
+    # Sending both Custom-Header-X and Custom-Header-Y but only Custom-Header-X is exposed
+    response.headers['Custom-Header-X'] = 'foo'
+    response.headers['Custom-Header-Y'] = 'bar'
+    return response
 
 
 @app.route("/ui", methods=["GET", "POST"])
@@ -86,22 +100,35 @@ def get_p8():
             logger.error("Failed getting private key. ")
     return p8
 
-def token_exchange(token, role, endpoint, snowflake_account_url):
-  scope_role = f'session:role:{role}'
-  scope = f'{scope_role} {endpoint}'
-  data = {
-    'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-    'scope': scope,
-    'assertion': token,
-  }
-  logger.info(data)
-  url = f'{snowflake_account_url}/oauth/token'
-  logger.info("oauth url: %s" %url)
-  response = requests.post(url, data=data, verify=False)
-  logger.info("snowflake jwt : %s" % response.text)
-  logger.info("response.status_code: %s" % response.status_code)
-  assert 200 == response.status_code, "unable to get snowflake token"
-  return response.text
+def token_exchange(token, role, endpoint, snowflake_account_url, isPat):
+    scope_role = f'session:role:{role}'
+    scope = f'{scope_role} {endpoint}'
+    
+    if isPat:
+        data = {
+            'grant_type': 'urn:ietf:params:oauth:grant-type:token-exchange',
+            'scope': scope,
+            'subject_token': token,
+            'subject_token_type': 'programmatic_access_token'
+        }
+    else:
+        data = {
+            'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+            'scope': scope,
+            'assertion': token,
+        }
+
+    logger.info(data)
+    response = _do_token_exchange(data, snowflake_account_url)
+    return response.text
+
+def _do_token_exchange(data, snowflake_account_url) -> requests.Response:
+    url = f'{snowflake_account_url}/oauth/token'
+    response = requests.post(url, data=data, verify=False)
+    logger.info("snowflake jwt response code : %s" % response.status_code)
+    assert 200 == response.status_code, "unable to get snowflake token"
+    return response
+
 
 @app.route('/jwt', methods=['POST'])
 def handle_data():
@@ -111,17 +138,21 @@ def handle_data():
         # Load the private key from the specified file.
         user = data.get("user")
         role = data.get("role")
+        isPat = data.get("isPat")
         snowflake_account_url = data.get("snowflake_account_url")
         snowflake_account_hostname = snowflake_account_url[8:]
         account = snowflake_account_hostname.split('.')[0]
         logger.debug(f'Account from Snowflake Account URL: {account}')
         endpoint = data.get("endpoint")
-        private_key_string = data.get("private_key_string")
-        token = JWTGenerator(account, user, private_key_string, timedelta(minutes=59),
-            timedelta(minutes=54)).get_token()
-        snowflake_jwt = token_exchange(token, role=role, endpoint=endpoint,
-                snowflake_account_url=snowflake_account_url)
-        logger.debug(f'snowflake_jwt: {snowflake_jwt}')
+        key = data.get("key")
+        if isPat:
+            snowflake_jwt = token_exchange(key, role=role, endpoint=endpoint,
+                    snowflake_account_url=snowflake_account_url, isPat=isPat)
+        else:
+            token = JWTGenerator(account, user, key, timedelta(minutes=59),
+                timedelta(minutes=54)).get_token()
+            snowflake_jwt = token_exchange(token, role=role, endpoint=endpoint,
+                    snowflake_account_url=snowflake_account_url, isPat=isPat)
         
         return snowflake_jwt
 
